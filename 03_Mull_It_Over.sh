cf438486c782e2eec0db561bed58f1353ea26ffc
David Mateos <dmateos@agresta.org>
2024-12-04 01:59:48 +0100

    dia 3 parte 2, ... con un poquito de perl para las lazy regex

--- 03_Mull_It_Over.sh
+++ 03_Mull_It_Over.sh
@@ -1,1 +1,6 @@
-cat 031_example_data | grep -oP "mul\([0-9]*,[0-9]*\)" | sed -e 's/[^0-9|,]//g' | gawk -F"," '{mul=$1*$2; sum+=mul} END{print sum;}'
+cat input/031_example_data | grep -oP "mul\([0-9]*,[0-9]*\)" | sed -e 's/[^0-9|,]//g' | gawk -F"," '{mul=$1*$2; sum+=mul} END{print sum;}'
+
+# Parte 2
+
+cat input/032_example_data | tr '\n' ' ' | perl -p -e  "s/don't().*?do\(\)//g" | grep -oP "mul\([0-9]*,[0-9]*\)" | sed -e 's/[^0-9|,]//g' | gawk -F"," '{mul=$1*$2; sum+=mul} END{print sum;}'
+
